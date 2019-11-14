fdbf7fedfacf15810c51cfc3a6893158fcce5de2
Logan-Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-11-14 15:26:01 -0800

    Add files via upload

---
+++ 5. 20_Animation_Cyber.py
@@ -0,0 +1,25 @@
+import turtle
+import math
+import random
+#global variable(s)
+#define global variables here
+#example:  xM = 0 
+
+def mike(t):
+	#change global variables
+        # example :  global xM = 200
+      add your code here
+
+def main():
+     try:
+          turtle.TurtleScreen._RUNNING = True
+          turtle.screensize(canvwidth=700, canvheight=700, bg=None)
+          print(turtle.Screen().screensize())
+          w = turtle.Screen()
+          mike(t)
+          w.exitonclick()
+     finally:
+          turtle.Terminator()
+	
+if __name__ == '__main__':
+	main()
