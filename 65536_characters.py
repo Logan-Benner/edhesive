f0bdb393f0f077e61da8e4395dd018763a09689c
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-12-13 10:29:43 -0800

    Update 65536_characters.py

--- 65536_characters.py
+++ 65536_characters.py
@@ -1,5 +1,5 @@
 import random
-for i in range (0,65536)
-d = random.randit(65, 65+26)
-t = chr(n)
-print(c,end="")
+for i in range (0,65536):
+ n = random.randint(65, 65+25)
+ c = chr(n) 
+ print(c,end="")
