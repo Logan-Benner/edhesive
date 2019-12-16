ec9aa9ac456d71147fcd5750c8ed8a60c1389e23
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-12-16 09:09:12 -0800

    Create algorithim_lb.py

---
+++ algorithim_lb.py
@@ -0,0 +1,13 @@
+# your code goes here
+i = 10
+if (i==10):
+	if (i < 15):
+		print ("i is samller than 15")
+		if (i < 12):
+			print ("i is samller than 12 too")
+		else:
+			print ("i is greater than 15")
+			if (i == 9):
+				print ("i is equal to 9")
+			else:
+				print ("is is not equal to 9")
