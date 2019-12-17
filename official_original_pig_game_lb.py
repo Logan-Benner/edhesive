20b47751335e07885fc19db55bbfe338e72c1ff7
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-12-17 10:21:15 -0800

    Add files via upload

---
+++ official_original_pig_game_lb.py
@@ -0,0 +1,21 @@
+#Official Pig Game
+#Pig Set Variables
+import random
+ 
+score = 0
+computerscore = 0
+humanscore = 0
+human = h
+computer = c
+roll = r
+keep = k
+players = ['h', 'c']
+	
+while():
+	for i in 'players':
+		choice = input("You are human, Roll(r) or Keep(k)?: ")
+	
+	
+main()
+  # The main fun
+     
