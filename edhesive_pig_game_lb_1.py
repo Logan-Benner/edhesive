c7491adeeec3faf6c20df24d0ba87f85cc99a2d2
Logan Benner <42662533+Logan-Benner@users.noreply.github.com>
2019-12-20 10:43:12 -0800

    Add files via upload

--- edhesive_pig_game_lb_1.py
+++ edhesive_pig_game_lb_1.py
@@ -1,20 +1,50 @@
 import random
 
 def main():
-	hscore = 0
 	cscore = 0
-	rounds = 0
+	hscore = 0
+	rounds = 1
 	
-	while (humanscore < 100 and computerscore < 100):
-		print("Round" + str(rounds))
-		hscore = dice_roll()
-		cscore = dice_roll()
-		print("human Roll: " + str(hscore))
-		print("computer  Roll: " + str(hscore))
+players = ['human', 'computer']
+random.shuffle(players)
+points = {'human': 0, 'computer': 0}
+	
+	
+	while True:
+		for i in players:
+			if i == 'human':
+				hscore = 0
+				cscore = 0
+				rounds = 1
+			while (hscore < 100 and cscore < 100):
+					print("Round " + str(rounds))
+					hscore = dice_roll()
+					cscore = dice_roll()
+					r = dice_roll
+					choice = input("You are human, Roll (r)")
+					if (choice=='r'):
+						while (rounds < 100):
+							if (rounds == 1):
+								hscore = 0
+								print("Rolled a", diceRoll)
+								print("Greedy piggy!")
+							else:
+								print("Not an input")
+					
+								break
+			
+								if (hscore == cscore):
+									print("Draw!")
+								elif hscore > cscore:
+									print("Human wins!")
+								else:
+									print("Computer wins!")
+					else:
 		
-		rounds = rounds + 1
+					rounds = rounds + 1
 
 def dice_roll():
-	diceRoll = random.randit(1,6)
+	diceRoll = random.randint(1,6)
 	return diceRoll
 	
+main()
